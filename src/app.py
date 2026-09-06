b9777ec711a063f69b7931a41cdb5d8acc193036
Hasinireddy10 <hasini10sweety@gmail.com>
2026-09-06 22:46:52 +0530

    converted to flask app

--- src/app.py
+++ src/app.py
@@ -1,10 +1,33 @@
+from flask import Flask
+app=Flask(__name__)
+
+
 def add(a,b):
     return a+b 
 
 def subtract(a,b):
     return a-b
 
-print(add(2,3))
-print(subtract(5,7))
+#print(add(2,3))
+#print(subtract(5,7))
+@app.route("/")
+def home():
+    return "Hello, this is my sample app for pipeline testing"
+@app.route("/add/<int:a>/<int:b>")
+def add_route(a, b):
+    return str(add(a, b))
+
+@app.route("/subtract/<int:a>/<int:b>")
+def subtract_route(a, b):
+    return str(subtract(a, b))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
 
 #ci cd automatic trigger test
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
-from src.app import add, subtract
+from src.app import add, subtract, app
+
 
 def test_add():
     assert add(2, 3) == 5
@@ -6,4 +7,8 @@ def test_add():
 def test_subtract():
     assert subtract(5, 7) == -2
 
-print(test_add())
+def test_home_route():
+    client=app.test_client()
+    response=client.get("/")
+    assert response.status_code==200
+    assert response.data== b"Hello, this is my sample app for pipeline testing"
